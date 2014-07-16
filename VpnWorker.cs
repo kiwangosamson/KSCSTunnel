de9e3c5b5216141b2e7f5f746ae3e08042addde4
teamalpha5441 <teamalpha5441@gmail.com>
2014-07-16 14:31:54 +0200

    Added VpnWorker and Packet classes

---
+++ VpnWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using TA.SharpTunnel.TunTap;
+using TA.SharpTunnel.Connections;
+
+namespace TA.SharpTunnel
+{
+	public class VpnWorker
+	{
+		private Connection _Connection;
+		private TunTapDevice _TunTap;
+		
+		public VpnWorker(Connection Connection, TunTapDevice TunTap)
+		{
+			_Connection = Connection;
+			_TunTap = TunTap;
+		}
+	}
+}
