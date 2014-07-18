eddac577411fcc40407d8d16fc8916ad09c05d0d
teamalpha5441 <teamalpha5441@gmail.com>
2014-07-18 07:57:49 +0200

    Improved authenticator system, added YesAuthenticator

---
+++ Authentication/Connector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TA.SharpTunnel.ModuleBaseClasses
+{
+	public abstract class ClientConnector : IDisposable
+    {
+        public abstract Stream Stream { get; }
+        
+		public abstract void Connect();
+		public abstract void Disconnect();
+		
+    	public virtual void Dispose()
+    	{
+    		try { Disconnect(); }
+    		catch { }
+    	}
+    }
+
+    public abstract class ServerConnector : IDisposable
+    {
+        public abstract Stream Stream { get; }
+        
+    	public abstract void Connect();
+		public abstract void Disconnect();
+
+    	public virtual void Dispose()
+    	{
+    		try { Disconnect(); }
+    		catch { }
+    	}
+    }
+}
---
+++ Authentication/YesAuthenticator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace TA.SharpTunnel.Authentication
+{
+    public class YesAuthenticator : Authenticator
+    {
+        public override byte[] ServerCreateChallenge(IPEndPoint Client) 
+        {
+            var challenge = new byte[1];
+            (new Random()).NextBytes(challenge);
+            return challenge;
+        }
+        
+        public override byte[] ClientCreateResponse(IPEndPoint Server, byte[] Challenge)
+        {
+            if (Challenge.Length != 1)
+                return new byte[1] { 0x00 };
+            else return new byte[1] { (byte)(255 - Challenge[0]) };
+        }
+        
+        public override bool ServerCheckResponse(IPEndPoint Client, byte[] Challenge, byte[] Response)
+        {
+            if (Challenge.Length != 1 || Response.Length != 1)
+                return false;
+            
+            return (255 - Response[0]) == Challenge[0];
+        }
+    }
+}
